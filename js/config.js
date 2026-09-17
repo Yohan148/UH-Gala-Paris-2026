24083353d3f95fd7cd3071cbe1f9657ce82d1e4a
Yohan148 <138446341+Yohan148@users.noreply.github.com>
2026-09-17 13:32:34 +0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.GALA_CONFIG = {
   googleScriptUrl: "https://script.google.com/macros/s/AKfycbz-clV_afRyOhfRzyx1PZn7dj1iFb1u7q4M1S1iWybe7Mqzso2Js4sxKituiFpD5u5M/exec",
-  allodonsUrl: "COLLE_ICI_TON_LIEN_ALLODONS"
+  allodonsUrl: "https://www.allodons.fr/dashboard/categories/24217/page"
 };
