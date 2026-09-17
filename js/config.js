218c9409f04f0dd584198d2cbb3ea90dc5349de6
Yohan148 <138446341+Yohan148@users.noreply.github.com>
2026-09-17 13:33:44 +0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.GALA_CONFIG = {
   googleScriptUrl: "https://script.google.com/macros/s/AKfycbz-clV_afRyOhfRzyx1PZn7dj1iFb1u7q4M1S1iWybe7Mqzso2Js4sxKituiFpD5u5M/exec",
-  allodonsUrl: "https://www.allodons.fr/dashboard/categories/24217/page"
+  allodonsUrl: "https://www.allodons.fr/hatzalah/categories/dons-page-pre-inscription-gala-paris-2026"
 };
